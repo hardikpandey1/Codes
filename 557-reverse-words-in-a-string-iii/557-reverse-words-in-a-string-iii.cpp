cdfec88f569ef89318766f6a750edd796fbdd99a
Hardik Pandey <70097454+hardikpandey1@users.noreply.github.com>
2022-02-24 03:49:40 +0530

    Time: 16 ms (76.84%), Space: 9.7 MB (75.11%) - LeetHub

---
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int l=0,r=0;
+        while(l<s.size()){
+            while(r<s.size() and s[r]!=' ')
+                r+=1;
+            reverse(s.begin()+l,s.begin()+r);
+            l=r+1;
+            r=l;
+            }
+        return s;
+    }
+};
