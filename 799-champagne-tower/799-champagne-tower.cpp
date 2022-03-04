5aad5ba6f8b067ab41ee893519b23bdd6694ec65
Hardik Pandey <70097454+hardikpandey1@users.noreply.github.com>
2022-03-05 05:17:50 +0530

    Time: 6 ms (80.67%), Space: 6.1 MB (75.00%) - LeetHub

--- 799-champagne-tower/799-champagne-tower.cpp
+++ 799-champagne-tower/799-champagne-tower.cpp
@@ -3,7 +3,7 @@ public:
     double champagneTower(int poured, int query_row, int query_glass) {
         double result[101][101]={0,0};
         result[0][0]=poured;
-        for(int i=0;i<100;i++){
+        for(int i=0;i<=query_row;i++){
             for(int j=0;j<=i;j++){
                 if(result[i][j]>=1){
                     result[i+1][j]+=(result[i][j]-1)/2.0;
