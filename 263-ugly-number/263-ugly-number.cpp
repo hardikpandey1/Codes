82566094f4f5e44a8f159ef00fc31e2470fa7bfa
Hardik Pandey <70097454+hardikpandey1@users.noreply.github.com>
2022-03-07 04:43:32 +0530

    Time: 0 ms (100.00%), Space: 6 MB (20.26%) - LeetHub

--- 263-ugly-number/263-ugly-number.cpp
+++ 263-ugly-number/263-ugly-number.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool isUgly(int n) {
+        if(n==0)
+            return false;
         for(int i=2;i<6 and n;i++)
             while(n%i==0)
                 n/=i;
