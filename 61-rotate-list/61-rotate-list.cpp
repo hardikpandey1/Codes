07b02ee2175dc6ec5319bc75dd0be867ac9b7858
Hardik Pandey <70097454+hardikpandey1@users.noreply.github.com>
2022-03-11 22:39:51 +0530

    Time: 8 ms (73.85%), Space: 11.8 MB (6.63%) - LeetHub

---
+++ 61-rotate-list/61-rotate-list.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head or !head->next or k==0)
+            return head;
+        ListNode *cur=head;
+        int l=1;
+        while(cur->next and l++)
+            cur=cur->next;
+        cur->next=head;
+        k%=l;
+        k=l-k;
+        while(k--)
+            cur=cur->next;
+        head=cur->next;
+        cur->next=NULL;
+        return head;
+    }
+};
