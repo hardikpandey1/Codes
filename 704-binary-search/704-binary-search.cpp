5af7cd579ea017f6ce0d418f18121b7c0362f529
Hardik Pandey <70097454+hardikpandey1@users.noreply.github.com>
2022-03-26 18:43:37 +0530

    Time: 47 ms (62.34%), Space: 27.5 MB (63.85%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int i=0,j=nums.size()-1,m;
+        while(i<=j){
+            m=i+(j-i)/2;
+            if(nums[m]==target)
+                return m;
+            else if(nums[m]<target)
+                i=m+1;
+            else
+                j=m-1;
+        }
+        return -1;
+    }
+};
