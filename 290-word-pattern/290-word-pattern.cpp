8ef86361132d2c0d16418872320f514a59242120
Hardik Pandey <70097454+hardikpandey1@users.noreply.github.com>
2022-04-04 16:18:28 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (99.69%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        unordered_map<char,int>p2i;
+        unordered_map<string,int>w2i;
+        istringstream in (s);string word;
+        int i=0;int n=pattern.size();
+        for(word;in>>word;i++){
+            if(i==n or p2i[pattern[i]]!=w2i[word])
+                return false;
+            p2i[pattern[i]]=w2i[word]=i+1;
+        }
+        return i==n;
+    }
+};
