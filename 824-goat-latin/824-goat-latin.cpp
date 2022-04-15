e4806e7334192878050bdde4b3adc847b79aa5d1
Hardik Pandey <70097454+hardikpandey1@users.noreply.github.com>
2022-04-15 18:14:19 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (18.95%) - LeetHub

---
+++ 824-goat-latin/824-goat-latin.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string toGoatLatin(string s) {
+        stringstream ss(s);
+        string result,word,suffix;
+        unordered_set<char>vowels={'a','e','i','o','u','A','E','I','O','U'};
+        while(ss>>word){
+            suffix+='a';
+            if(vowels.find(word[0])==vowels.end())
+                word=word.substr(1)+word[0];
+            result+=word+"ma"+suffix+' ';
+        }
+        result.pop_back();
+        return result;
+    }
+};
