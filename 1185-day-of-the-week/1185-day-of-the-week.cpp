2be728d641a54ddc2fa3bc36b6a7a1187c1deec0
Hardik Pandey <70097454+hardikpandey1@users.noreply.github.com>
2022-05-02 16:15:51 +0530

    Time: 0 ms (100.00%), Space: 6 MB (60.92%) - LeetHub

---
+++ 1185-day-of-the-week/1185-day-of-the-week.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string dayOfTheWeek(int d, int m, int y) {
+        if(m==1 or m==2)
+        {
+            m+=12;
+            y-=1;
+        }
+        int z=(d+2*m+3*(m+1)/5+y+y/4-y/100+y/400+1)%7;
+        if(z==0)
+            return "Sunday";
+        else if(z==1)
+            return "Monday";
+        else if(z==2)
+            return "Tuesday";
+        else if(z==3)
+            return "Wednesday";
+        else if(z==4)
+            return "Thursday";
+        else if(z==5)
+            return "Friday";
+        else
+            return "Saturday";
+    }
+};
