eb2e2e816663eb0f00c7e4b17aea00986ec43373
Hardik Pandey <70097454+hardikpandey1@users.noreply.github.com>
2022-05-15 00:10:09 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (48.97%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -4,7 +4,7 @@ public:
         if(needle.size()==0)
             return 0;
         for(int i=0;i<haystack.size();i++){
-            if(haystack[i]==needle[0]){
+            //if(haystack[i]==needle[0]){
                 for(int j=0;j<needle.size();j++){
                     if(haystack[i+j]==needle[j]){
                         if(j==needle.size()-1){
@@ -15,7 +15,7 @@ public:
                         break;
                 }
             }
-        }
+        
         return -1;
     }
 };
