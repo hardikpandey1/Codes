a1b4acf480865634419bbbb74be192d3f9bcc377
Hardik Pandey <70097454+hardikpandey1@users.noreply.github.com>
2022-05-30 23:46:45 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (77.67%) - LeetHub

---
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        if(s.size()==0)
+            return 0;
+        int i=0;
+        int j=s.size()-1;
+        while(i<j){
+            if(s[i]==s[j])
+            {
+                i+=1;
+                j-=1;
+            }
+            else
+                return 2;
+        }
+        return 1;
+    }
+};
