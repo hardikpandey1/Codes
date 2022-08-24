fdb426b1a2099ac487d7b2adf6356581fca45b93
Hardik Pandey <70097454+hardikpandey1@users.noreply.github.com>
2022-08-25 02:53:28 +0530

    Time: 8 ms (11.91%), Space: 8.4 MB (23.98%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-    void sortColors(vector<int>& a) {
-        int z=0,o=0,t=0;
-        int n=a.size();
-  for(int i=0;i<n;i++){
-    if(a[i]==0)
-      z+=1;
-    else if(a[i]==1)
-      o+=1;
-    else
-      t+=1;
-  }
-  int i=0;
-  while(z){
-    a[i++]=0;
-    z-=1;
-  }
-  while(o){
-    a[i++]=1;
-    o-=1;
-  }
-  while(t){
-    a[i++]=2;
-    t-=1;
-  }
+    void sortColors(vector<int>& nums) {
+        int l=0;
+        int h=nums.size()-1;
+        int m=0;
+        while(m<=h){
+        switch(nums[m]){
+            case 0:
+                swap(nums[l++],nums[m++]);
+                break;
+            case 1:
+                m+=1;
+                break;
+            case 2:
+                swap(nums[m],nums[h--]);
+        }
+    }
     }
 };
