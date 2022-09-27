8467230875ee32ebbba9f6444e003a8285fdf293
Hardik Pandey <70097454+hardikpandey1@users.noreply.github.com>
2022-09-28 00:48:15 +0530

    Time: 518 ms (34.70%), Space: 16.4 MB (45.05%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        sort(begin(nums),end(nums));
-        int diff=INT_MAX;
-        int ans=0;
-        for(int i=0;i<nums.size();i++){
-            int first=nums[i];
-            int start=i+1;
-            int end=nums.size()-1;
-            while(start<end){
-                if(first+nums[start]+nums[end] == target)
-                    return target;
-                else if(abs(first+nums[start]+nums[end]-target)<diff){
-                    diff=abs(first+nums[start]+nums[end]-target);
-                    ans=first+nums[start]+nums[end];
-                }
-                else if(first+nums[start]+nums[end] > target)
-                    end-=1;
-                else if(first+nums[start]+nums[end] < target)
-                    start+=1;
+        sort(nums.begin(),nums.end());
+        int closestSum=1000000000;
+        for(int i=0;i<nums.size()-2;i++){
+            int ptr1=i+1,ptr2=nums.size()-1;
+            while(ptr1<ptr2){
+                int sum=nums[i]+nums[ptr1]+nums[ptr2];
+                if(sum==target)
+                    return sum;
+                if(abs(target-sum)<abs(target-closestSum))
+                    closestSum=sum;
+                if(sum>target)
+                    ptr2-=1;
+                else
+                    ptr1+=1;
             }
         }
-        return ans;
+        return closestSum;
     }
 };
